c4e09b3c1d2483a8a7bfc8932d2403445e29ba69
issy20 <gggibraltar86@gmail.com>
2021-06-11 17:15:50 +0900

    change allow host

--- daily_api/settings.py
+++ daily_api/settings.py
@@ -31,8 +31,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['djnago-daily.herokuapp.com']
-
+ALLOWED_HOSTS = ['djnago-daily-api.herokuapp.com']
 
 # Application definition
 
